fa431e7cc1fd7824062889b8431887a42ac8d8fc
Alexandre Benkendorf <alexandre.benkendorf@visma.com>
2026-04-17 15:42:43 +0100

    refactor: move tsconfig files into tsconfig/ directory
    - Move tsconfig.base.json, tsconfig.node.json, tsconfig.eslint.json into tsconfig/
    - Update internal extends paths to be relative within tsconfig/
    - Update tsconfig.json to extend ./tsconfig/base.json
    - Update eslint.config.mjs project path to ./tsconfig/eslint.json
    - Update package.json exports and files to reflect new paths
    - Remove published tsconfig.json from exports (consumers extend tsconfig/base.json directly)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import createEslintConfig from './eslint/createEslintConfig.mjs';
 
 export default await createEslintConfig({
-  project: ['./tsconfig.eslint.json'],
+  project: ['./tsconfig/eslint.json'],
   tsconfigRootDir: import.meta.dirname,
   includeImport: false,
   includeReact: false,
